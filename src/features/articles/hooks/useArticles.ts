33f3108e977156af343cc7228e91a0b9a877df7c
singgihseptiann <septiansinggih93@gmail.com>
2025-04-28 17:46:22 +0700

    feat: delete articles by id

--- src/features/articles/hooks/useArticles.ts
+++ src/features/articles/hooks/useArticles.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import api from "@/service/service";
 import { usePagination } from "@/context/pagination.context";
 
@@ -9,5 +9,21 @@ export const useArticles = () => {
     queryKey: ["articles", page],
     queryFn: () => api.get("articles", { page, limit }),
     staleTime: 5 * 60 * 1000,
+  });
+};
+
+export const useDeleteArticles = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (id: string) => api.delete("articles", id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["articles"] });
+      alert("Artikel berhasil dihapus");
+    },
+    onError: (error) => {
+      console.error("Error:", error);
+      alert("Terjadi kesalahan, coba lagi nanti!");
+    },
   });
 };
